a0787a1c29093f7eb01fb6470e83c9c889003ab4
b1urr <93118753+0xblurr@users.noreply.github.com>
2021-12-07 15:59:44 -0700

    Create ex2-1.c

---
+++ Chapter 2/ex2-1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+
+
+int main(void){
+    printf("Unsigned Char: [%d, %d]\n", 0, UCHAR_MAX);
+    printf("Signed Char: [%d, %d]\n", SCHAR_MIN, SCHAR_MAX);  
+    printf("Unsigned short: [%d , %d]\n", 0, USHRT_MAX);
+    printf("Signed short: [%d , %d]\n", SHRT_MIN, SHRT_MAX);
+    printf("Unsigned int: [%d , %d]\n", 0, UINT_MAX);
+    printf("Signed int: [%d , %d]\n", INT_MIN, INT_MAX);
+    printf("Unsigned long: [%d , %d]\n", 0, ULONG_MAX);
+    printf("Signed long: [%d , %d]\n", LONG_MIN, LONG_MAX);
+
+    /*Not sure how to compute the above values. I will return to add once I figure that out.*/
+
+
+
+    return 0;
+}
