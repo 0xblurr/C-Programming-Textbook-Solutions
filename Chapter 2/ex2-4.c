b56a7f3c03b10512fee762639ced277c5611228d
b1urr <93118753+0xblurr@users.noreply.github.com>
2021-12-14 13:08:43 -0700

    Add files via upload

---
+++ Chapter 2/ex2-4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define MAXLINE 1000
+
+
+void squeeze(char s1[], char s2[]);
+int getline(char s[], int lim);
+
+int main(void){
+
+    char testStringOne[MAXLINE] = "Delete occurences of same letter.";
+    char testStringTwo[MAXLINE] = "This line has some of the same letters.";
+
+    squeeze(testStringOne, testStringTwo);
+    printf("%s", testStringOne);
+
+    
+
+    return 0;
+}
+
+void squeeze(char s1[], char s2[]){
+    int c1;
+    for(int i = 0; s1[i] != '\0'; i++){
+        c1 = s1[i];
+        for(int k = 0; s2[k] != '\0'; k++){
+            if(s2[k] == c1){
+                s1[i] = ' ';
+            }
+        }
+    }
+}
+
