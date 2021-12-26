56203c6590bc013b0d443054edd04748e76b0382
blurr <93118753+0xblurr@users.noreply.github.com>
2021-12-26 14:24:02 -0700

    Add files via upload

---
+++ Chapter 4/ex4-6/ex4-6_2.h
@@ -0,0 +1,35 @@
+#include <ctype.h>
+#include<stdio.h>
+
+#define NUMBER '0'
+#define VARGET 'a'
+char var = '0';
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next character or numeric operand */
+int getop(char s[]){
+   
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if(isalpha(c)){
+        var = c;
+        return VARGET;
+    }
+    if (!isdigit(c) && c != '.')
+        return c; /* not a number */
+    i = 0;
+    if (isdigit(c)) /* collect integer part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.') /* collect fraction part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
