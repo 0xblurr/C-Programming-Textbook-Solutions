3e2c0062bd3131b3292e34f393714a63fc835be7
blurr <93118753+0xblurr@users.noreply.github.com>
2021-12-28 17:00:18 -0700

    Add files via upload

---
+++ Chapter 4/ex4-3.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <ctype.h>
+#define MAXOP 100 /* max size of operand or operator */
+#define NUMBER '0' /* signal that a number was found */
+#define MAXVAL 100 
+#define BUFSIZE 100
+
+
+int sp = 0; 
+double val[MAXVAL]; 
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+
+
+int getop(char []);
+void push(double);
+double pop(void);
+int getch(void);
+void ungetch(int);
+
+
+/* reverse Polish calculator */
+int main(void){
+    int type, modOp2;
+    double op2;
+    char s[MAXOP];
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                int op1;
+                op1 = pop();
+                int OP2 = pop();
+                printf("op1: %d op2: %d\n", op1, OP2);
+                push(op1 + OP2);
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '%':
+                modOp2 = pop();
+                if(modOp2 != 0.0)
+                    push((int)pop() % modOp2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+    return 0;
+}
+
+
+
+void push(double f){
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+    }
+
+
+double pop(void){
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+int getop(char s[]){
+   
+    int i = 0, c, next;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c; /* not a number */
+    if(c == '-'){
+        next = getch();
+        if(!isdigit(next) && next != '.'){
+            return next;
+        }
+
+        s[i] = c;
+        ungetch(c = next);
+    }
+    else{
+        c = getch();
+    }  
+    if (isdigit(c)) /* collect integer part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.') /* collect fraction part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+int getch(void){
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
