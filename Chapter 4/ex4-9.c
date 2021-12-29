72590339e662873c60800cd2260f31a412652968
blurr <93118753+0xblurr@users.noreply.github.com>
2021-12-29 11:46:48 -0700

    Add files via upload

---
+++ Chapter 4/ex4-9.c
@@ -0,0 +1,9 @@
+int main(void){
+
+    /*The compiler I use on Windows uses signed chars by default. Therefore,
+    the existing code handles EOF correctly since the char array can hold negative
+    integers. I'm not sure how to implement a platform independent version but will
+    do so in the future.*/
+
+    return 0;
+}
